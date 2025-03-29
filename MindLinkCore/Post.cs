943950e82a4f85ea6bec88997a1535067f25b6ac
Mohamed Youssef <mohamed.youssef@agile-architecture.com>
2025-03-29 02:39:51 +0200

    folders are converted to libraries

---
+++ MindLinkCore/Post.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MindLinkCore
+{
+    class Post
+    {
+        public int MyProperty { get; set; }
+        public int MyProperty1 { get; set; }
+        public int MyProperty2 { get; set; }
+
+    }
+}
