c0621fec004844313d2df6483ad6cd744aaddf09
hajar <h.mohamed2255@nu.edu.eg>
2025-09-06 04:42:09 +0300

    New LogIn & SignUp (:

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './feed/styles.css';
-import App from './feed/Feed';
+import './LogIn/styles.css';
+import LoginSign from './LogIn/LoginSign';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <LoginSign />
   </React.StrictMode>
 );
 
